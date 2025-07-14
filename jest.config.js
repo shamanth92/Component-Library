d8ff40e11e30df7179116529cb945bbbdbdfce38
Shamanth Kumar Parameshwar <shamanth.parameshwar@xcelenergy.com>
2025-07-14 18:22:23 -0500

    fix jest config again

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./../my-component-library",
+  dir: "./",
 });
 
 const config = {
