bcb9d008180811e487512e9df563900a2636e59d
Shamanth Kumar Parameshwar <shamanth.parameshwar@xcelenergy.com>
2025-07-15 14:16:15 -0500

    Complete button test cases

--- __tests__/button.test.tsx
+++ __tests__/button.test.tsx
@@ -1,22 +1,82 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Button, BUTTONTYPES } from "./../src/components/button";
 
-describe("Page", () => {
-  it("renders a heading", () => {
-    render(
-      <Button
-        variant={BUTTONTYPES.Contained}
-        onClick={() => console.log("Button Clicked")}
-      >
-        CONTAINED
-      </Button>
-    );
+describe("Button", () => {
+  it("renders a contained button", () => {
+    render(<Button variant={BUTTONTYPES.Contained}>CONTAINED</Button>);
 
     const button = screen.getByRole("button", {
       name: "CONTAINED",
     });
 
     expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("bg-sky-500");
+    expect(button).toHaveClass("text-white");
+  });
+
+  it("renders a contained button", () => {
+    render(<Button variant={BUTTONTYPES.Simple}>SIMPLE</Button>);
+
+    const button = screen.getByRole("button", {
+      name: "SIMPLE",
+    });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("renders a red outlined button", () => {
+    render(
+      <Button variant={BUTTONTYPES.Outlined} color="red">
+        OUTLINED
+      </Button>
+    );
+
+    const button = screen.getByRole("button", {
+      name: "OUTLINED",
+    });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("border-red-500");
+    expect(button).toHaveClass("text-red-500");
+  });
+
+  it("renders a disabled button", () => {
+    render(
+      <Button variant={BUTTONTYPES.Disabled} color="red">
+        Disabled
+      </Button>
+    );
+
+    const button = screen.getByRole("button", {
+      name: "Disabled",
+    });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("bg-gray-500");
+    expect(button).toHaveClass("cursor-not-allowed");
+  });
+
+  it("renders a contained button and tests the on click function", () => {
+    render(
+      <Button variant={BUTTONTYPES.Contained} onClick={() => testClick()}>
+        Contained
+      </Button>
+    );
+
+    const logSpy = jest.spyOn(console, "log").mockImplementation();
+
+    const testClick = () => {
+      console.log("abcd");
+    };
+
+    const button = screen.getByRole("button", {
+      name: "Contained",
+    });
+
+    fireEvent.click(button);
+
+    expect(button).toBeInTheDocument();
+    expect(logSpy).toHaveBeenCalledWith("abcd");
   });
 });
